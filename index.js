decda07798950f8d7bd4214b4cd808605171dead
henryezikeai <henryezike@gmail.com>
2025-08-15 17:42:16 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+let burgers = ['Hamburger', 'Cheeseburger']
+let featuredDrink = 'Strawberry Milkshake'
+
+function newBurger(){
+    let newBurger = 'Flatburger'
+    burgers.push(newBurger)
+}
+
+if (true) {
+    anothernewBurger = 'Maple Bacon Burger'
+    burgers.push(anothernewBurger)
+}
+
+function changeFeaturedDrink(){
+featuredDrink = 'The Javashake'
+}
+
+changeFeaturedDrink()
+console.log(featuredDrink)
+console.log(burgers)
